e8f035d4dc1ea0db83af31e77a9a856a445626f0
pipecogram69 <102176178+pipecogram69@users.noreply.github.com>
2022-04-05 16:38:23 -0500

    Add files via upload

---
+++ src/main/java/Gestionapto.java
@@ -0,0 +1,21 @@
+
+import java.sql.Date;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author RYZEN 5
+ */
+public class Gestionapto extends Usuario {
+    public  Usuario usuario;
+    public Date fecha;
+    
+
+    
+    
+}
---
+++ src/main/java/Usuario.java
@@ -0,0 +1,21 @@
+
+import java.sql.Date;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author RYZEN 5
+ */
+public class Usuario {
+    public String  Nombre;
+    public String Identificacion;
+    public String  Deuda;
+    public Date fecha;
+    
+    
+}
